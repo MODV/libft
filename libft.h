d7e19a5b4e3d0bd0a5658a013e953103a31934c9
Volcov Oleg <vivk15@mail.ru>
2017-11-12 18:36:21 +0000

    test 001

--- libft.h
+++ libft.h
@@ -28,15 +28,11 @@ void	            ft_putnbr_fd(int n, int fd);
 void	            ft_putstr(char const *str);
 void	            ft_putstr_fd(char const *str, int fd);
 
-/*+++++++MEM--------*/
+/*-------MEM--------*/
 
 void                *ft_memset(void *str, int c, size_t n);
 void                ft_bzero(void *s, size_t n);
 void                *ft_memcpy(void *dest, const void *src, size_t n);
-void	            *ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	            *ft_memmove(void *dst, const void *src, size_t len);
-int                 ft_memcmp(const void *s1, const void *s2, size_t n);
-void	            *ft_memchr(const void *s, int c, size_t n);
 void	            *ft_memalloc(size_t n);
 void	            ft_memdel(void **f);
 
@@ -44,13 +40,6 @@ void	            ft_memdel(void **f);
 
 size_t              ft_strlen (char const *str);
 char                *ft_strdup(const char *s);
-void	            *ft_strnew(size_t size);
-char                *ft_strjoin(char const *s1, char const *s2);
-void	            ft_strclr(char *s);
-int                 ft_strcmp(const char *s1, const char *s2);
-void                ft_strdel(char **as);
-int                 ft_strequ(char const *s1, char const *s2);
-int                 ft_strncmp(const char *s1, const char *s2, size_t n);
 
 /*+++++CONVERT-----*/
 
